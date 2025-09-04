072cd1af4384b45b36163238daf8b29a1e958b9f
Jaime de Oliveira <jaaiimeee91@gmail.com>
2025-09-03 21:39:22 -0300

    class event

--- src/main/java/com/mycompany/pratique/jaime/silva/Event.java
+++ src/main/java/com/mycompany/pratique/jaime/silva/Event.java
@@ -1,26 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.pratique.jaime.silva;
 
-/**
- *
- * @author jaime
- */
-public class Event {
+import java.io.Serializable;
+
+public class Event implements Serializable {
     private String titulo;
+    private String data;
+    private String local;
+    private User organizador;
 
-    public Event(String titulo) {
+    public Event(String titulo, String data, String local, User organizador) {
         this.titulo = titulo;
+        this.data = data;
+        this.local = local;
+        this.organizador = organizador;
     }
 
     public String getTitulo() {
         return titulo;
     }
 
+    public String getData() {
+        return data;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public User getOrganizador() {
+        return organizador;
+    }
+
     @Override
     public String toString() {
-        return titulo;
+        return titulo + " | " + data + " | " + local + " | Organizador: " + organizador.getNome();
     }
 }
